4ddb7fe1f1ce704488f9c59b7682f435c6878742
Nirvan Tyagi <nirvan.tyagi@gmail.com>
2019-04-30 20:06:43 -0400

    Redis read key get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+
+use rocket_contrib::databases::redis;
+use rocket_contrib::databases::redis::Commands;
+
+#[database("redis")]
+struct DbConn(redis::Connection);
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!\n"
+}
+
+#[get("/<key>")]
+fn get_key(conn: DbConn, key: String) -> String {
+    let value: Result<String, redis::RedisError> = conn.get(key);
+    match value {
+        Ok(v) => format!("{}\n", v),
+        Err(e) => format!("Key does not exist: {}\n", e)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .attach(DbConn::fairing())
+        .mount("/", routes![index, get_key])
+        .launch();
 }
